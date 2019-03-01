e13008bb8944d11b1f7043737c494172729407e3
THOM BULLOCK <t@thomascbullock.com>
2019-03-01 07:29:31 -0800

    adding question 5

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -110,10 +110,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+    var multTotal = 1;
+    for (var i = 0; i < multArr.length; i++) {
+        multTotal = multiply(multTotal,multArr[i])[0];
+    }
+    return [multTotal, "The numbers " + multArr + " have a product of " + multTotal + "."];
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
