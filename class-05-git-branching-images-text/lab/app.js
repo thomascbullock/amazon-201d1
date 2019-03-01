cb9357f3212b326f6e9ac26ac63be1306c801bba
THOM BULLOCK <t@thomascbullock.com>
2019-02-28 18:38:18 -0800

    answered wrong question. Commiting question two now...

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -29,7 +29,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    var result = parseInt(a) * parseInt(b);
+    var resultStr = "The product of " + a + " and " + b + " is " + result + ".";
+    return [result, resultStr];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -50,13 +52,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    var result = parseInt(a) * parseInt(b);
-    var resultStr = "The product of " + a + " and " + b + " is " + result + ".";
-    return [result, resultStr];
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
  testSumAndMultiply(4,7,5);
+
+ 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
