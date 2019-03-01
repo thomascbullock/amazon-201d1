095856c3829f967124b048312348af4eab86ac55
THOM BULLOCK <t@thomascbullock.com>
2019-03-01 07:24:37 -0800

    adding question four

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -52,11 +52,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var sumResult = sum(sum(a,b)[0],c)[0];
+    
+    var multiplyResult =  multiply(multiply(a,b)[0],c)[0];
 
+    var sumStr = a + " and " + b + " and " + c + " sum to " + sumResult + ".";
+
+    var multiplyStr = "The product of " + a + " and " + b + " and " + c + " is " + multiplyResult + ".";
+    
+    return [sumResult, multiplyResult, sumStr, multiplyStr];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -75,12 +83,17 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    var sumTotal = 0;
+    for (var i = 0; i < sumArr.length; i++) {
+        sumTotal = sum(sumTotal,sumArr[i])[0];
+    }
+    return [sumTotal, sumArr + " was passed in as an array of numbers, and " + sumTotal + " is their sum."];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
